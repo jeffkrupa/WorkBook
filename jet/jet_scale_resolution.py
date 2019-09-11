de0ce4e2b37eed138cbe7d4d36b0c736cdd398d1
Zeynep <zeynep.demiragli@gmail.com>
2019-09-11 14:44:04 -0500

    simple and convolution fits

--- jet/jet_scale_resolution.py
+++ jet/jet_scale_resolution.py
@@ -21,15 +21,16 @@ def main():
     parser.add_argument("-o", "--output"  ,dest="output"  , help="output folder name", type=str)
     parser.add_argument("-e", "--eta"     ,dest="eta"     , help="define the eta range, examples are '0_1p3','1p3_2p1',2p1_2p5','2p5_3p0', '3p0_5p0'",type=str)
     parser.add_argument("-n", "--npv"     ,dest="npv"     , help="define the npv range, examples are '0_70','0_10','10_30','30_70'",type=str)
+    parser.add_argument("-s", "--simple"  ,dest="simple"  , help="To enable convolution fit, set it to 0",type=int)
     args = parser.parse_args()    
 
     f_in = ROOT.TFile(str(args.input),"READ")
     t_in = f_in.Get("events")
-    _pt  = [30,50,75,100,150,250,500,1000]
+    _pt  = [50,75,100,150,250,500,1000]
     _eta = [float(args.eta.split("_")[0].replace("p",".")), float(args.eta.split("_")[1].replace("p","."))]
     _npv = [float(args.npv.split("_")[0]), float(args.npv.split("_")[1])]
 
-    folder = args.output+"/"
+    folder = args.output+"_"+str(args.simple)+"/"
     os.system("mkdir -p "+folder)
     os.system("mkdir -p "+folder+"/fit")
 
@@ -46,7 +47,13 @@ def main():
         npv_bin = "npv>="+str(_npv[0])+" && npv<"+str(_npv[1])
         t_in.Draw("rawjet_pt/genjet_pt>>shape",reco_bin + "&& " + gen_bin + "&&" + eta_bin + "&&" + npv_bin,"goff")
 
-        mean, mean_error, sigma, sigma_error = ConvFit(shape ,False,"ratio","jet pt/gen jet pt",folder+"/fit","FIT_pt_"+str(_pt[i])+"_eta_"+args.eta+"_npv_"+args.npv)
+        if args.simple is 1:
+            mean = shape.GetMean()
+            mean_error = shape.GetMeanError()
+            sigma = shape.GetRMS()
+            sigma_error = shape.GetRMSError()
+        else:
+            mean, mean_error, sigma, sigma_error = ConvFit(shape ,False,"ratio","jet pt/gen jet pt",folder+"/fit","FIT_pt_"+str(_pt[i])+"_eta_"+args.eta+"_npv_"+args.npv)
 
         h_mean.SetBinContent(i+1,mean)
         h_mean.SetBinError(i+1,mean_error)
@@ -60,94 +67,59 @@ def main():
         h_sigma.SetBinContent(i+1,sigma)
         h_sigma.SetBinError(i+1,sigma_error)
 
-        c = ROOT.TCanvas("mean","mean", 600, 600)
-        c.cd()
-        c.SetLogx()
-        ROOT.gStyle.SetOptStat(False)
-        h_mean.SetTitle("")
-        h_mean.GetXaxis().SetTitle("Gen jet p_{T} [GeV]")
-        h_mean.GetYaxis().SetTitle("Response")
-        h_mean.GetXaxis().SetTitleOffset(1.2)
-        h_mean.GetYaxis().SetTitleOffset(1.3)
-        h_mean.SetMaximum(1.3)
-        h_mean.SetMinimum(0.7)
-        h_mean.SetLineWidth(2)
-        h_mean.Draw("")
-        h_mean.SetMarkerStyle(20)
-        h_mean.SetMarkerSize(0.8)
-        
-        legend = ROOT.TLegend(0.50, 0.65, 0.65, .85);
-        legend . AddEntry(h_mean,"chs jet" , "lp")
+    c = ROOT.TCanvas("mean","mean", 600, 600)
+    c.cd()
+    c.SetLogx()
+    ROOT.gStyle.SetOptStat(False)
 
-        legend.Draw("same")
+    legend = ROOT.TLegend(0.50, 0.55, 0.65, .65);
+    legend . AddEntry(h_mean,"chs jet" , "lp")
 
-        latex2 = ROOT.TLatex()
-        latex2.SetNDC()
-        latex2.SetTextSize(0.4*c.GetTopMargin())
-        latex2.SetTextFont(42)
-        latex2.SetTextAlign(31) # align right                                                     
-        latex2.DrawLatex(0.90, 0.93,str(_eta[0])+" < #eta <"+ str(_eta[1]) + ", "+ str(_npv[0])+" < npv < " + str(_npv[1]))
+    latex2 = ROOT.TLatex()
+    latex2.SetNDC()
+    latex2.SetTextSize(0.4*c.GetTopMargin())
+    latex2.SetTextFont(42)
+    latex2.SetTextAlign(31) # align right                                                     
+    latex2.DrawLatex(0.90, 0.93,str(_eta[0])+" < #eta <"+ str(_eta[1]) + ", "+ str(_npv[0])+" < npv < " + str(_npv[1]))
 
-        latex2.Draw("same")
+    makePlot(h_mean, "Gen jet p_{T} [GeV]", "Response")
+    legend.Draw("same")
+    latex2.Draw("same")
+    c.SaveAs(folder+"FIT_mean_eta_"+args.eta+"_npv_"+args.npv+".png")
+    c.SaveAs(folder+"FIT_mean_eta_"+args.eta+"_npv_"+args.npv+".pdf")
+    
+    c1 = ROOT.TCanvas("sigma","sigma", 600, 600)
+    c1.cd()
+    c1.SetLogx()
 
-        c.SaveAs(folder+"FIT_mean_eta_"+args.eta+"_npv_"+args.npv+".png")
-        c.SaveAs(folder+"FIT_mean_eta_"+args.eta+"_npv_"+args.npv+".pdf")
+    makePlot(h_sigma_corrected,"Gen jet p_{T} [GeV]","Resolution / Response")
+    latex2.Draw("same")
+    legend.Draw("same")
+    c1.SaveAs(folder+"FIT_sigma_eta_"+args.eta+"_npv_"+args.npv+".png")
+    c1.SaveAs(folder+"FIT_sigma_eta_"+args.eta+"_npv_"+args.npv+".pdf")
+    
+    c2 = ROOT.TCanvas("sigmaabs","sigmaabs", 600, 600)
+    c2.cd()
+    c2.SetLogx()
+    makePlot(h_sigma,"Gen jet p_{T} [GeV]","Resolution")
+    latex2.Draw("same")
+    legend.Draw("same")
+    c2.SaveAs(folder+"FIT_sigmaabs_eta_"+args.eta+"_npv_"+args.npv+".png")
+    c2.SaveAs(folder+"FIT_sigmaabs_eta_"+args.eta+"_npv_"+args.npv+".pdf")
 
-        c1 = ROOT.TCanvas("sigma","sigma", 600, 600)
-        c1.cd()
-        c1.SetLogx()
-        h_sigma_corrected.GetXaxis().SetTitle("Gen jet p_{T} [GeV]")
-        h_sigma_corrected.GetYaxis().SetTitle("Resolution / Response")
-        h_sigma_corrected.SetTitle("")
-        h_sigma_corrected.GetXaxis().SetTitleOffset(1.2)
-        h_sigma_corrected.GetYaxis().SetTitleOffset(1.5)
-        h_sigma_corrected.SetLineWidth(2)
-        h_sigma_corrected.SetMaximum(0.5)
-        h_sigma_corrected.SetMinimum(0)
-        h_sigma_corrected.Draw("")
-        h_sigma_corrected.SetMarkerStyle(20)
-        h_sigma_corrected.SetMarkerSize(0.8)
+def makePlot(hist, xtitle, ytitle):
 
-        latex2 = ROOT.TLatex()
-        latex2.SetNDC()
-        latex2.SetTextSize(0.4*c1.GetTopMargin())
-        latex2.SetTextFont(42)
-        latex2.SetTextAlign(31) # align right                                                     
-        latex2.DrawLatex(0.90, 0.93,str(_eta[0])+" < #eta <"+ str(_eta[1]) + ", "+ str(_npv[0])+" < npv < " + str(_npv[1]))
-
-        latex2.Draw("same")
-
-        legend.Draw("same")
-        c1.SaveAs(folder+"FIT_sigma_eta_"+args.eta+"_npv_"+args.npv+".png")
-        c1.SaveAs(folder+"FIT_sigma_eta_"+args.eta+"_npv_"+args.npv+".pdf")
-
-        c2 = ROOT.TCanvas("sigmaabs","sigmaabs", 600, 600)
-        c2.cd()
-        c2.SetLogx()
-        h_sigma.GetXaxis().SetTitle("Gen jet p_{T} [GeV]")        
-        h_sigma.GetYaxis().SetTitle("Resolution")
-        h_sigma.SetTitle("")
-        h_sigma.GetXaxis().SetTitleOffset(1.2)
-        h_sigma.GetYaxis().SetTitleOffset(1.3)
-        h_sigma.SetLineWidth(2)
-        h_sigma.SetMaximum(0.5)
-        h_sigma.SetMinimum(0)
-        h_sigma.SetMarkerStyle(20)
-        h_sigma.SetMarkerSize(0.8)
-        h_sigma.Draw("")
-
-        latex2 = ROOT.TLatex()
-        latex2.SetNDC()
-        latex2.SetTextSize(0.4*c2.GetTopMargin())
-        latex2.SetTextFont(42)
-        latex2.SetTextAlign(31) # align right                                                     
-        latex2.DrawLatex(0.90, 0.93,str(_eta[0])+" < #eta <"+ str(_eta[1]) + ", "+ str(_npv[0])+" < npv < " + str(_npv[1]))
-
-        latex2.Draw("same")
-
-        legend.Draw("same")
-        c2.SaveAs(folder+"FIT_sigmaabs_eta_"+args.eta+"_npv_"+args.npv+".png")
-        c2.SaveAs(folder+"FIT_sigmaabs_eta_"+args.eta+"_npv_"+args.npv+".pdf")
+    hist.SetTitle("")
+    hist.GetXaxis().SetTitle(xtitle)
+    hist.GetYaxis().SetTitle(ytitle)
+    hist.GetXaxis().SetTitleOffset(1.2)
+    hist.GetYaxis().SetTitleOffset(1.3)
+    hist.SetMaximum(1.3)
+    hist.SetMinimum(0.7)
+    hist.SetLineWidth(2)
+    hist.Draw("")
+    hist.SetMarkerStyle(20)
+    hist.SetMarkerSize(0.8)
 
 if __name__ == '__main__':
     main()
